84aa7f60cac8a683b7dcd3e421133fc095d3bbd4
eda127 <127062@epsstudents.org>
2025-04-02 14:30:25 -0400

    Add files via upload

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -2,7 +2,7 @@ public class Bulbasaur extends Pokemon{
   
     public Bulbasaur(){
       super("Bulbasaur", 50, 10, 15, "Grass", new Move[] {
-                      new Move("tackle", 8, "Normal"), 
+                      new Move("tackle", 8, "Normal", 1,95), 
                       new Move("tail whip", 0, "Normal"),
                       new Move("vine whip", 10, "Grass"),
                       new Move("bite", 10, "Normal")});
--- Pidgey.java
+++ Pidgey.java
@@ -1,9 +1,9 @@
 public class Pidgey extends Pokemon{
     public Pidgey(){
         super("Pidgey", 40, 10, 15, "Flying", new Move[] {
-                        new Move("tackle", 8, "Normal"), 
-                        new Move("gust", 10, "Flying"),
-                        new Move("sand attack", 0, "Ground"),
-                        new Move("quick attack", 4, "Normal")});
+                        new Move("tackle", 8, "Normal", 1), 
+                        new Move("gust", 10, "Flying", 1, 95),
+                        new Move("sand attack", 0, "Ground", 1),
+                        new Move("quick attack", 4, "Normal", 1, 3)});
       }
     }
