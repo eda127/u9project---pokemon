781a0926ca6afa9bae2b584ea44239c1bbdb8f3e
eda127 <127062@epsstudents.org>
2025-04-04 14:26:30 -0400

    Add files via upload

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -3,9 +3,9 @@ public class Bulbasaur extends Pokemon{
     public Bulbasaur(){
       super("Bulbasaur", 50, 10, 15, "Grass", new Move[] {
                       new Move("tackle", 8, "Normal", 1,95), 
-                      new Move("tail whip", 0, "Normal"),
-                      new Move("vine whip", 10, "Grass"),
-                      new Move("bite", 10, "Normal")});
+                      new Move("tail whip", 0, "Normal", 1),
+                      new Move("vine whip", 10, "Grass", 3),
+                      new Move("bite", 10, "Normal", 1)});
     }
   
   }
