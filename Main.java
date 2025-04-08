9e6142f04baec206a5b2b0aa890a016edc2c30cb
eda127 <127062@epsstudents.org>
2025-04-07 21:33:11 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,13 +3,16 @@ public class Main {
   public static void main(String[] args) {
     Bulbasaur b = new Bulbasaur();
     Pidgey p = new Pidgey();
+
     Lucario l = new Lucario();
     Jolteon j = new Jolteon();
     Flygon f = new Flygon();
     Pelipper pp = new Pelipper();
     onlyStatus o = new onlyStatus();
+    Raticate r = new Raticate();
+    Poliwrath pr = new Poliwrath();
 
-    Battle battle = new Battle(p, l);
+    Battle battle = new Battle(pr, j);
     battle.start();
     }
 
